21cfd6c851f58babcccba9b1aa983d1f75b22a88
Kévin Reilhac <kevin.reilhac.pro@gmail.com>
2022-06-20 23:34:09 +0200

    Add panel containers

---
+++ Runtime/baseUIPanelContainer.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kebab.UISystem
+{
+	public class baseUIPanelContainer : baseUIPanel
+	{
+		List<baseUIPanel> subPanels = new List<baseUIPanel>();
+
+		override public void Init()
+		{
+			subPanels = gameObject.GetComponentsInChildren<baseUIPanel>(true).ToList();
+		}
+
+		override public void Show()
+		{
+			subPanels.ForEach((p) => p.Show());
+		}
+
+		override public void Hide()
+		{
+			subPanels.ForEach((p) => p.Hide());
+		}
+	}
+}
